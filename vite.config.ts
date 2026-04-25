4e8ee0a5b7615911980c438325ef0f578855e07a
Med-Taher-Hlioui <medtaherhlioui@gmail.com>
2026-04-25 10:34:40 +0100

    fix: final paths and netlify headers

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <--- This must be here
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // <--- This must be here
+    tailwindcss(),
   ],
-  base: './',
+  base: '/', // Change this to a single forward slash
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
 })
